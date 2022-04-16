f355c4fd5d8d589f76f4d8a99230181ec8b9e415
Abdulhafiz <hafeez_96@hotmail.com>
2022-04-16 23:52:41 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+const driversArr = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+]; 
+const findMatching = (arr, str) => arr.filter(a => a.toUpperCase() == str.toUpperCase())
+const fuzzyMatch = (arr, str) => arr.filter(a => a[0] == str[0])
+const matchName = (arr, str) => arr.filter((a => a.name == str))
+console.log(matchName(drivers, "Bobby"))
